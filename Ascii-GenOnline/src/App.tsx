499cf07498a37aec9c039ab31b9dfde4e669784b
ioloEJ42 <ioloevans42@gmail.com>
2024-08-09 17:57:50 +0100

    Init

---
+++ Ascii-GenOnline/src/App.tsx
@@ -0,0 +1,11 @@
+import ASCIIArtGenerator from './ASCIIArtGenerator'; 
+
+function App() {
+  return (
+    <div className="App">
+      <ASCIIArtGenerator />
+    </div>
+  );
+}
+
+export default App;
